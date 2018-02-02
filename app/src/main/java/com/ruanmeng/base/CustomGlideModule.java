907544d335cb905b24f899c67f917a60b7cb58c9
xinnuo <416143467@qq.com>
2018-02-02 11:08:26 +0800

    update 02-02/11:08

--- app/src/main/java/com/ruanmeng/base/CustomGlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.ruanmeng.base;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public class CustomGlideModule extends AppGlideModule { }
